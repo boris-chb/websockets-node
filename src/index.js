2cf16bcc11e43117088e9eff31e95826ce01f2ba
Boris <47457848+boris-chb@users.noreply.github.com>
2023-08-30 02:28:11 +0100

    added event handlers

--- src/index.js
+++ src/index.js
@@ -12,42 +12,54 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("user connected");
+io.on("connection", socket => {
+  console.log("user connected", socket.client.request.socket.remoteAddress);
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-
-  socket.on("new video", (metadata) => {
+  socket.on("new video", metadata => {
     io.emit("new video", metadata);
-  });
-
-  socket.on("save", (msg) => {
-    io.emit("save", msg);
-  });
-
-  socket.on("submit", (msg) => {
-    io.emit("submit", msg);
-  });
-
-  socket.on("strike", (msg) => {
-    io.emit("strike", msg);
   });
 
   socket.on("approve", ({ language }) => {
     io.emit("approve", { language });
   });
 
+  socket.on("route", ({ target }) => {
+    io.emit("route", { target });
+  });
+
+  socket.on("strike", msg => {
+    io.emit("strike", msg);
+  });
+
+  socket.on("save", () => {
+    io.emit("save");
+  });
+
+  socket.on("submit", () => {
+    io.emit("submit");
+  });
+
   socket.on("delete review", () => {
     io.emit("delete review");
+  });
+
+  socket.on("seek to", timestampStr => {
+    io.emit("seek to", timestampStr);
+  });
+
+  socket.on("add note", note => {
+    io.emit("add note", note);
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
+  socket.on("chat message", msg => {
+    io.emit("chat message", msg);
+  });
 });
 
-server.listen(3000, () => {
-  console.log("listening on localhost:3000");
+server.listen(3001, () => {
+  console.log("listening on localhost:3001");
 });
